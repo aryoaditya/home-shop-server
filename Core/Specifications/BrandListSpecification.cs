0be2de6bf2eea5836ca13101bdb55531f619a2f9
Aryo Aditya <aryoaditya2000@gmail.com>
2024-10-22 10:37:05 +0700

    feat: add specification for filtering and sorting

---
+++ Core/Specifications/BrandListSpecification.cs
@@ -0,0 +1,10 @@
+using Core.Models;
+namespace Core.Specifications;
+public class BrandListSpecification : BaseSpecification<Product, string>
+{
+    public BrandListSpecification()
+    {
+        AddSelect(x => x.Brand);
+        ApplyDistinct();
+    }
+}
---
+++ Core/Specifications/TypeListSpecification.cs
@@ -0,0 +1,10 @@
+using Core.Models;
+namespace Core.Specifications;
+public class TypeListSpecification : BaseSpecification<Product, string>
+{
+    public TypeListSpecification()
+    {
+        AddSelect(x => x.Type);
+        ApplyDistinct();
+    }
+}
